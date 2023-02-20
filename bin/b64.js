2109dc90de3b2547414d3ae53b4906850ba72090
Ramses Garate <47763122+ramsesgarate@users.noreply.github.com>
2023-02-20 13:16:58 -0300

    feat: add main command file

---
+++ bin/b64.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { version } = require('../package.json');
+
+const program = new Command();
+
+program
+  .name('b64')
+  .description('validate, encode and decode in Base64')
+  .version(version)
+  .command('validate', 'Validate base64')
+  .alias('v')
+  .command('encode', 'Encode in base64')
+  .alias('e')
+  .command('decode', 'Decode base64')
+  .alias('d');
+
+program.parse(process.argv);
