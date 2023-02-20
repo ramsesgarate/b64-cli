a4ae7f18aa42ff4a8805ade5636fd22df519a054
Ramses Garate <47763122+ramsesgarate@users.noreply.github.com>
2023-02-20 13:17:21 -0300

    feat: add command for validate base64

---
+++ bin/b64-validate.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import chalk from 'chalk';
+import { isValid } from 'js-base64';
+
+const program = new Command();
+
+program.argument('<str>', 'String to validate').parse(process.argv);
+
+const [str] = program.args;
+
+const isValidBase64 = isValid(str);
+const validationText = isValidBase64
+  ? chalk.green('valid')
+  : chalk.red('invalid');
+
+const output = `
+Base64 is ${validationText}
+`;
+
+console.log(output);
