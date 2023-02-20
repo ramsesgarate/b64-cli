6af8f4c35213a713c917ea6bd5ad83cac8297850
Ramses Garate <47763122+ramsesgarate@users.noreply.github.com>
2023-02-20 13:17:44 -0300

    feat: add command for encoded string to base64

---
+++ bin/b64-encode.js
@@ -0,0 +1,20 @@
+import { Command } from 'commander';
+import { encode } from 'js-base64';
+import chalk from 'chalk';
+import clipboardy from 'clipboardy';
+
+const program = new Command();
+
+program.argument('<str>', 'Str enconde to Base64').parse(process.argv);
+
+const [str] = program.args;
+
+const strEncode = encode(str);
+const strPretty = chalk.green(strEncode);
+
+const output = `
+${chalk.blue(str)} enconde to Base64 ${strPretty}
+`;
+
+clipboardy.writeSync(strEncode);
+console.log(output);
